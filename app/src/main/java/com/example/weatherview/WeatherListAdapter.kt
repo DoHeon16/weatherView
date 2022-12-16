27449ed05353685f3e440e77347db7940a73efb2
DoHeon16 <58592231+DoHeon16@users.noreply.github.com>
2022-12-16 14:42:49 +0900

    ninth commit

--- app/src/main/java/com/example/weatherview/WeatherListAdapter.kt
+++ app/src/main/java/com/example/weatherview/WeatherListAdapter.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class WeatherListAdapter(val weatherList:List<Weather>)
-    :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    :RecyclerView.Adapter<WeatherListAdapter.ViewHolder>(){
 
     class ViewHolder(view:View):RecyclerView.ViewHolder(view){
         var image:ImageView
@@ -26,32 +26,20 @@ class WeatherListAdapter(val weatherList:List<Weather>)
             rain=view.findViewById(R.id.RN1)
         }
 
-        fun bind(weather: Weather){
-            place.text=weather.place
-            temp.text=weather.temperature
-            humi.text=weather.humidity
-            rain.text=weather.rainfall
-        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v= LayoutInflater.from(parent.context).inflate(R.layout.weatherbox,parent,false)
         return ViewHolder(v)
     }
 
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        if(weatherList[position].x=="59"&&weatherList[position].y=="125"){
-//            holder.place.text="동작구"
-//            holder.temp.text=weatherList[position].temperature
-//            holder.humi.text=weatherList[position].humidity
-//            holder.rain.text=weatherList[position].rainfall
-//        }
-//    }
-
     override fun getItemCount(): Int {
         return weatherList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(weatherList[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.place.text=weatherList[position].place
+        holder.temp.text=weatherList[position].temperature
+        holder.humi.text=weatherList[position].humidity
+        holder.rain.text=weatherList[position].rainfall
     }
 }
